fba55d76beb19fa0753320f9b985a04fb789f377
duonanotte <166713796+duonanotte@users.noreply.github.com>
2024-10-15 13:45:03 +0300

    Update main.py

--- main.py
+++ main.py
@@ -1,22 +1,39 @@
 import asyncio
 import sys
+import os
+import signal
 
 from bot.utils.banner import banner
-from bot.utils import logger
+from bot.utils.logger import logger
 from bot.utils.launcher import process
-from bot.utils.connection_manager import manage_connections
+from bot.utils.connection_manager import connection_manager
 
-@manage_connections
+def suppress_errors():
+    sys.stderr = open(os.devnull, 'w')
+
 async def main():
-    await process()
+    try:
+        await process()
+    except asyncio.CancelledError:
+        pass
+    finally:
+        await connection_manager.close_all()
+
+def signal_handler(signum, frame):
+    sys.exit(0)
 
 
 if __name__ == '__main__':
     banner()
+    signal.signal(signal.SIGINT, signal_handler)
+    signal.signal(signal.SIGTERM, signal_handler)
+
     try:
         asyncio.run(main())
     except KeyboardInterrupt:
         pass
+    except SystemExit:
+        pass
     finally:
-        logger.info("<lr>Bot stopped by user.</lr>")
-        sys.exit(0)
+        logger.info("<lr>Bot stopped by user</lr>")
+        suppress_errors()
